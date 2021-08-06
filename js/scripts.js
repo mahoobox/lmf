0f7a078dc6855ebfb093cc5bab73f6d58194936a
MahooBox <mauricio@nebra.co>
2021-08-06 15:10:53 -0500

    comercios

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-/*!
-* Start Bootstrap - Shop Item v5.0.2 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+function miFuncion() {
+    whatsappTitulo = "Escribeme a Whatsapp ";
+    $('#header').load('./../header.html');
+    $('#footer').load('./../footer.html');
+    document.getElementById('whatsappBoton').innerHTML=whatsappTitulo;
+  }
+window.onload=miFuncion;
+
+  
